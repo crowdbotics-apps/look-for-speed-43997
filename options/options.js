72a6a6536e7165d89f53940d1cdb78910471eb87
Crowdbotics <team@crowdbotics.com>
2023-10-18 00:29:33 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "look-for-speed-43997",
+  url: "https://look-for-speed-43997.botics.co",
+  api: "https://look-for-speed-43997.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
